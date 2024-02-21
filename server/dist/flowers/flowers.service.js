3d1590fab4166d24579568fb2d6858ba706b90d1
Kirill Rozhkov <kirillr648@gmail.com>
2024-02-21 20:28:57 +0300

    fix:flower is create update delete findone find all

--- server/dist/flowers/flowers.service.js
+++ server/dist/flowers/flowers.service.js
@@ -5,28 +5,67 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.FlowersService = void 0;
 const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
+const flower_entity_1 = require("./entities/flower.entity");
+const typeorm_2 = require("typeorm");
 let FlowersService = class FlowersService {
-    create(createFlowerDto) {
-        return 'This action adds a new flower';
+    constructor(flowerRepository) {
+        this.flowerRepository = flowerRepository;
     }
-    findAll() {
-        return `This action returns all flowers`;
+    async create(createFlowerDto) {
+        const flower = await this.flowerRepository.save({
+            name: createFlowerDto.name,
+            price: createFlowerDto.price,
+            quantity: createFlowerDto.quantity,
+            image: createFlowerDto.image,
+            description: createFlowerDto.description,
+            country: createFlowerDto.country,
+            year: createFlowerDto.year,
+            model: createFlowerDto.model
+        });
+        return { flower };
     }
-    findOne(id) {
-        return `This action returns a #${id} flower`;
+    async findAll() {
+        return await this.flowerRepository.find({});
     }
-    update(id, updateFlowerDto) {
-        return `This action updates a #${id} flower`;
+    async findOne(id) {
+        const flower = await this.flowerRepository.findOne({
+            where: { id }
+        });
+        if (!flower)
+            throw new common_1.NotFoundException("Цветы не найдены");
+        return flower;
     }
-    remove(id) {
-        return `This action removes a #${id} flower`;
+    async update(id, updateFlowerDto) {
+        const flower = await this.flowerRepository.findOne({
+            where: { id }
+        });
+        if (!flower)
+            throw new common_1.NotFoundException("Цветы не найдены");
+        return await this.flowerRepository.update(id, updateFlowerDto);
+    }
+    async remove(id) {
+        const flower = await this.flowerRepository.findOne({
+            where: { id }
+        });
+        if (!flower)
+            throw new common_1.NotFoundException("Цветы не найдены");
+        return await this.flowerRepository.delete(id);
     }
 };
 exports.FlowersService = FlowersService;
 exports.FlowersService = FlowersService = __decorate([
-    (0, common_1.Injectable)()
+    (0, common_1.Injectable)(),
+    __param(0, (0, typeorm_1.InjectRepository)(flower_entity_1.Flower)),
+    __metadata("design:paramtypes", [typeorm_2.Repository])
 ], FlowersService);
 //# sourceMappingURL=flowers.service.js.map
